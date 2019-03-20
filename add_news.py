bd52da508598736315d91b38959cfe5df33cd385
Andrey Bobrovskiy <bobr.200@yandex.ru>
2019-03-20 20:59:05 +0300

    add add new

--- add_news.py
+++ add_news.py
@@ -4,6 +4,5 @@ from wtforms.validators import DataRequired
 
 
 class AddNewsForm(FlaskForm):
-    title = StringField('Заголовок новости', validators=[DataRequired()])
-    content = TextAreaField('Текст новости', validators=[DataRequired()])
-    submit = SubmitField('Добавить')
+    content = TextAreaField('Text', validators=[DataRequired()])
+    submit = SubmitField('Add')
